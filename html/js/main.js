a27d46fd8a5e7a082f0a57b3cbc21d7abb924d7e
Jairo Merea <jairo.merea.2019@gmail.com>
2021-01-06 00:03:02 -0500

    I finished the design and I started to put some js code

--- html/js/main.js
+++ html/js/main.js
@@ -1,4 +1,6 @@
 let listSelector = document.querySelector("#select");
+let listSpanSelector = document.querySelector("#select .searching-Bar_List_select_content span");
+let inputList = document.querySelector("#inputRegion");
 let optionsSelector = document.querySelector("#options");
 let modeSelector = document.querySelector("#mode");
 let headerSelector = document.querySelector("#header");
@@ -8,6 +10,10 @@ let searchBarListSelector = document.querySelector("#search-Bar-List");
 let searchBarImputSelector = document.querySelector("#search-Bar-Imput");
 let contentSelector = document.querySelector("#content");
 let optiontSelector = document.querySelector("#option");
+let popSelector = document.querySelector("#pop");
+let backButtonSelector = document.querySelector("#pop .popHeader .backButton");
+let contryListSelector = document.querySelectorAll("#country-list .country-list__container")
+let contryListH3Selector = document.querySelectorAll("#country-list .country-list__container .country-list__container_description h3")
 
 select.addEventListener("click", ()=> {
     options.classList.toggle("active");
@@ -22,5 +28,26 @@ mode.addEventListener("click", () =>{
     optionsSelector.classList.toggle("mode-White-List")
     contentSelector.classList.toggle("mode-white-content")
     optiontSelector.classList.toggle("mode-White-List")
+    popSelector.classList.toggle("mode-White")
 } )
 
+document.querySelectorAll(".hover-list span").forEach((opcion) => {
+    opcion.addEventListener("click", (e) => {
+        e.preventDefault();
+        listSpanSelector.innerHTML = e.currentTarget.innerHTML;
+        inputList.value = e.currentTarget.innerHTML;
+        options.classList.toggle("active");
+    })
+})
+
+contryListSelector.forEach((country) => {
+    country.addEventListener("click", () => {
+        popSelector.classList.toggle("active")
+        contentSelector.classList.toggle("none")
+    })
+})
+
+backButtonSelector.addEventListener("click", ()=>{
+    popSelector.classList.toggle("active")
+    contentSelector.classList.toggle("none")
+})
